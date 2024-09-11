8e36cf154eb8794206102a7b76da24601ccf1347
Andwele Ancheta <andwele.ancheta@gmail.com>
2024-09-11 18:40:02 +1000

    Combined Navigation Bar and CoverImage into Header Component

--- Langwarrin_CC/src/pages/Forms.jsx
+++ Langwarrin_CC/src/pages/Forms.jsx
@@ -1,14 +1,12 @@
 //import React from "react";
+import Header from "../components/Header";
 import Footer from "../components/Footer";
-import CoverImage from "../components/CoverImage";
-import NavigationBar from "../components/Navigation_Bar";
 
 
 function Forms() {
     return (
         <div>
-            <NavigationBar></NavigationBar>
-            <CoverImage/>
+            <Header/>
             <h1>Forms</h1>
             <Footer/>
         </div>
--- Langwarrin_CC/src/pages/Services.jsx
+++ Langwarrin_CC/src/pages/Services.jsx
@@ -1,14 +1,11 @@
-//import React from "react";
+import Header from "../components/Header";
 import Footer from "../components/Footer";
-import CoverImage from "../components/CoverImage";
-import NavigationBar from "../components/Navigation_Bar";
 
 
 function Services() {
     return (
         <div>
-            <NavigationBar></NavigationBar>
-            <CoverImage/>
+            <Header/>
             <h1>Services</h1>
             <Footer/>
         </div>
