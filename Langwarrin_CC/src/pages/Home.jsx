79397e9f02b6686f491f4036174cd9570ef6178f
Andwele Ancheta <andwele.ancheta@gmail.com>
2024-09-15 14:31:56 +1000

    Add information into footer component

--- Langwarrin_CC/src/pages/Home.jsx
+++ Langwarrin_CC/src/pages/Home.jsx
@@ -3,6 +3,7 @@
 import Header from "../components/Header";
 import HomeCarousel from "../components/Carousel";
 import HomeCards from "../components/HomeCards";
+import Footer from "../components/Footer";
 
 
 function Home() {
@@ -11,6 +12,7 @@ function Home() {
             <Header/>
             <HomeCards/>
             <HomeCarousel/>
+            <Footer/>
         </div>
     );
 }
