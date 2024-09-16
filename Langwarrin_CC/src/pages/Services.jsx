266d573fb486b73432af7a78bb71beb362b5b320
cyapharis <cyapharis@student.unimelb.edu.au>
2024-09-16 17:30:36 +1000

    added functional form page and created brochure button in service page

--- Langwarrin_CC/src/pages/Services.jsx
+++ Langwarrin_CC/src/pages/Services.jsx
@@ -1,7 +1,11 @@
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import ServicesGrid from "../components/ServicesGrid";
+import ServicesButton from "../components/ServicesButton";
 
+import Brochure from '../assets/icons8-brochure-100.png';
+
+const BrochureLink =  '/'
 
 
 function Services() {
@@ -21,6 +25,12 @@ function Services() {
         <ServicesGrid Summary/>
         
         
+        <article><center> <br/><br/>Current Brochure</center></article>
+        <div className="text"><center> 
+            Detailed information about our services can be found on our brochure
+            <br/><br/>
+        </center></div>
+        <ServicesButton Logo={Brochure} Link={BrochureLink}/>
         <Footer/>
             
     </div>
