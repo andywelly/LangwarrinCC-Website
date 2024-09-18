265996de90d51812830c079669bf3454367b5477
Andwele Ancheta <andwele.ancheta@gmail.com>
2024-09-18 16:42:00 +1000

    Fixed Github pages Deployment issues

--- Langwarrin_CC/vite.config.js
+++ Langwarrin_CC/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Langwarrin-CC/',
+  base: process.env.NODE_ENV === 'production' ? '/Langwarrin-CC/' : '/',
   plugins: [react()],
 })
